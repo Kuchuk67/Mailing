8b399635b6567e59db3236107cbb3db80b5ad5c9
Serg_Nout <kuchukov.sergey@gmail.com>
2025-02-05 07:37:20 +0300

    fixed

--- mailing/views/analitic_views.py
+++ mailing/views/analitic_views.py
@@ -12,9 +12,17 @@ class AnaliticView(View):
         count_clients = ClientName.objects.count()
         count_clients_active = ClientName.objects.filter(unsubscribe=0).count()
         clients_unsubscribe = count_clients - count_clients_active
-
-
+        # количество рассылок
+        count_tasks_stop = Task.objects.filter(status='stop').count()
+        count_tasks = Task.objects.filter().count()
+        count_tasks_end = Task.objects.filter(status='end').count()
+        count_tasks_active = count_tasks - (count_tasks_end + count_tasks_stop)
         return render(request, 'mailing/index.html',
                       {'count_clients': count_clients,
                        'clients_unsubscribe': clients_unsubscribe,
-                       'count_clients_active' : count_clients_active})
+                       'count_clients_active' : count_clients_active,
+                       'count_tasks_stop': count_tasks_stop,
+                       'count_tasks': count_tasks,
+                       'count_tasks_end': count_tasks_end,
+                       'count_tasks_active': count_tasks_active,
+                       })
